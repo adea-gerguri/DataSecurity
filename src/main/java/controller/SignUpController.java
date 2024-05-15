b32898088c3905f16c88d19fd3588de598c0ff32
adeat <adeatabaku@student.uni-pr.edu>
2024-05-15 21:37:46 +0200

    Add SignUpController

--- src/main/java/controller/SignUpController.java
+++ src/main/java/controller/SignUpController.java
@@ -1,4 +1,63 @@
 package controller;
 
+import app.Navigator;
+import database.DatabaseUtil;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.TextField;
+import model.dto.UserDto;
+import service.DBConnector;
+import service.UserService;
+
+import java.sql.*;
+
 public class SignUpController {
+    @FXML
+    private TextField txtFirstName;
+    @FXML
+    private TextField txtLastName;
+    @FXML
+    private TextField txtEmail;
+    @FXML
+    private PasswordField pwdPassword;
+    @FXML
+    private PasswordField pwdConfirmPassword;
+
+    @FXML
+    private void handleSignUp(ActionEvent ae) {
+        String firstName = txtFirstName.getText();
+        String lastName = txtLastName.getText();
+        String email = txtEmail.getText();
+        String password = pwdPassword.getText();
+        String confirmPassword = pwdConfirmPassword.getText();
+
+        if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+            System.out.println("Please fill in all the fields.");
+            return;
+        }
+
+        if (!password.equals(confirmPassword)) {
+            System.out.println("Password and confirm password do not match.");
+            return;
+        }
+
+        UserDto userSignUpData = new UserDto(firstName, lastName, email, password, confirmPassword);
+
+
+        boolean response = UserService.signUp(userSignUpData);
+        System.out.println("Response: " + response);
+
+
+        Navigator.navigate(ae, Navigator.LOGIN_PAGE);
+    }
+    @FXML
+    private void handleCancel(ActionEvent ae) {
+        txtFirstName.clear();
+        txtLastName.clear();
+        txtEmail.clear();
+        pwdPassword.clear();
+        pwdConfirmPassword.clear();
+    }
 }
